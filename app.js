b29f9e737272b61397bdb4f33225add2d32fbd29
Developer-Parth <166241612+Developer-Parth@users.noreply.github.com>
2025-03-16 15:06:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+document.getElementById('intro').style.opacity = "1";
+setTimeout(() => {
+    document.getElementById('intro').style.opacity = "0";
+    document.getElementById('intro').style.transition = "opacity 1s ease-out";
+    setTimeout(() => {
+        document.getElementById('intro').style.display = "none";
+    }, 1000);
+}, 1000);
+
 const wrapper = document.querySelector('.sliderWrapper');
 
 const menuItems = document.querySelectorAll('.menuItem');
